70fa5245c3303906d6a2aff01b5974f2ed4129a7
NTIIPB <38815500+BIGBunane@users.noreply.github.com>
2019-11-25 11:15:03 +0800

    上传练习
    上传了一个JAVA文档

---
+++ ForForDemo.java
@@ -0,0 +1,13 @@
+public class ForForDemo{
+	public static void main(String[] args){
+		for(int hour=0;hour<24;hour++){
+			for(int minute=0;minute<60;minute++){
+				System.out.println(hour+"Ê±"+minute+"·Ö");
+			
+			}
+		
+		}
+		
+	}
+	
+}
